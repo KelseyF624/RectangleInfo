db62775150b13fb70f1260919b11048effa5fdb7
KelseyF624 <kelseyf.624@gmail.com>
2025-02-17 14:17:07 -0500

    Partial Commit

--- src/RectangleInfo.java
+++ src/RectangleInfo.java
@@ -1,15 +1,17 @@
 import java.util.Scanner;
 public class RectangleInfo {
     public static void main(String[] args) {
+        double width;
         do {
             System.out.println("Please enter the width of the rectangle.");
             Scanner sc = new Scanner(System.in);
-            double width = sc.nextInt();
+            width = sc.nextInt();
         }while (width > 0);
+        double length;
         do {
             System.out.println("Please enter the length of the rectangle.");
             Scanner sc1 = new Scanner(System.in);
-            double length = sc1.nextInt();
+            length = sc1.nextInt();
         }while (length > 0);
     }
 }
