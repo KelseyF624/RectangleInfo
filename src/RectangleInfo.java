1317085dc9c3527b2d72f6255d912f24943755c4
KelseyF624 <kelseyf.624@gmail.com>
2025-02-17 13:47:38 -0500

    Partial Commit

--- src/RectangleInfo.java
+++ src/RectangleInfo.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class Main {
+public class RectangleInfo {
     public static void main(String[] args) {
         do {
             System.out.println("Please enter the width of the rectangle.");
