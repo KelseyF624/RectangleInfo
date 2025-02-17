7092bdc56073c1e866b7143e34071a7ded61ee78
KelseyF624 <kelseyf.624@gmail.com>
2025-02-17 15:15:43 -0500

    Complete Commit

--- src/RectangleInfo.java
+++ src/RectangleInfo.java
@@ -6,12 +6,18 @@ public class RectangleInfo {
             System.out.println("Please enter the width of the rectangle.");
             Scanner sc = new Scanner(System.in);
             width = sc.nextInt();
-        }while (width > 0);
+        }while (width <= 0);
         double length;
         do {
             System.out.println("Please enter the length of the rectangle.");
             Scanner sc1 = new Scanner(System.in);
             length = sc1.nextInt();
-        }while (length > 0);
+        }while (length <= 0);
+        double area = width * length;
+        double perimeter = 2 * width * length;
+        double diagonalLength = Math.sqrt(area * area / perimeter);
+        System.out.println("The area of the rectangle is " + area);
+        System.out.println("The perimeter of the rectangle is " + perimeter);
+        System.out.println("The diagonal length of the rectangle is " + diagonalLength);
     }
 }
